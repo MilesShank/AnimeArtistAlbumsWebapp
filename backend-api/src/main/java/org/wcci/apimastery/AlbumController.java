152811504f709d9318267e32c98874ea9fd66eb1
Mignonne Silva <mignonne.n.silva@gmail.com>
2020-07-13 14:59:16 -0400

    Added album controller and mapping

---
+++ backend-api/src/main/java/org/wcci/apimastery/AlbumController.java
@@ -0,0 +1,36 @@
+package org.wcci.apimastery;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+public class AlbumController {
+
+    AlbumStorage albumStorage;
+
+    public AlbumController(AlbumStorage albumStorage){
+        this.albumStorage = albumStorage;
+    }
+
+    @GetMapping("/api/albums/")
+    public Collection<Album> retrieveAllAlbums(){
+        return albumStorage.retrieveAllAlbums();
+    }
+
+    @GetMapping("/api/album/{id}/")
+    public Album retrieveAlbumById(@PathVariable long id){
+        return albumStorage.retrieveAlbumById(id);
+    }
+    @DeleteMapping("/api/albums/{id}/")
+    public Collection<Album> deleteAlbum(@PathVariable long id){
+        albumStorage.delete(id);
+        return albumStorage.retrieveAllAlbums();
+
+    }
+
+
+}
--- backend-api/src/main/java/org/wcci/apimastery/AlbumStorage.java
+++ backend-api/src/main/java/org/wcci/apimastery/AlbumStorage.java
@@ -2,9 +2,18 @@ package org.wcci.apimastery;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 @Service
 public class AlbumStorage {
     private AlbumRepository albumRepo;
+
+public Collection<Album> retrieveAllAlbums(){
+    return (Collection<Album>) albumRepo.findAll();
+}
+public Album retrieveAlbumById(long id){
+    return albumRepo.findById(id).get();
+}
 
     public AlbumStorage(AlbumRepository albumRepo) {
         this.albumRepo = albumRepo;
@@ -12,4 +21,10 @@ public class AlbumStorage {
     public Album save(Album album){
         return albumRepo.save(album);
     }
+
+    public void delete(long id){
+    albumRepo.deleteById(id);
+    }
+
+
 }
