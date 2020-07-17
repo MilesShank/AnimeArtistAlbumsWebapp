7d7cef04a9055bdd99f4ddcf129f6f4ba6859378
JGibson37 <Jason.Gibson15@yahoo.com>
2020-07-17 14:13:00 -0400

    single album photo, and songs display

---
+++ frontend-spa/src/js/components/displayOneAlbum.js
@@ -0,0 +1,42 @@
+import { 
+    clearElementChildren 
+} from "../domHelper.js"
+
+export {
+    displayOneAlbum
+}
+
+
+
+const displayOneAlbum = (artistContainer, album) =>{
+    alert(`you clicked on ${album.title}`)
+    clearElementChildren(artistContainer)
+
+    const artistInfo = document.createElement("section")
+    artistInfo.classList.add("artist-info")
+
+    const div = document.createElement("div")
+    div.classList.add("artist-grid")
+    artistInfo.append(div)
+    artistContainer.append(artistInfo)
+
+    const h1 = document.createElement("h1")
+    div.append(h1)
+    h1.innerHTML=`${album.title}`
+
+    const div2 = document.createElement("div")
+    div2.classList = "artist-grid-item2"
+    artistInfo.append(div2)
+    const albumImg = document.createElement("img")
+    div2.append(albumImg)
+    albumImg.src= album.imageSource
+    albumImg.classList = "single-artist-art"
+
+    const ol = document.createElement("ol")
+    album.songs.forEach(song => {
+        const li = document.createElement("li")
+        li.innerHTML= `<p>${song.songTitle}</p>`
+        ol.append(li)
+    });
+    div.append(ol)
+}
