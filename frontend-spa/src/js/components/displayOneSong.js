2201a3599af97c56be6e1dce1efaa5bb3a23a5d0
MilesInSpace <Cshank.public@gmail.com>
2020-07-21 14:26:12 -0400

    added displayOneSong component, embeded video

---
+++ frontend-spa/src/js/components/displayOneSong.js
@@ -0,0 +1,35 @@
+import { 
+    clearElementChildren 
+} from "../domHelper.js"
+export{
+    displayOneSong
+}
+
+const displayOneSong = (artistContainer, song, album) =>{
+    alert(`you clicked on ${song.songTitle}`)
+    clearElementChildren(artistContainer);
+    artistContainer.classList.add("song-container");
+
+    const div  = document.createElement("div");
+    div.classList.add("song-details");
+    const ul = document.createElement("ul");
+    ul.innerHTML = `
+    <li>${song.songTitle}</li>
+    <li>${song.songDuration}</li>
+    <li>${album.title}</li>
+
+    `
+    div.append(ul);
+    artistContainer.append(div);
+
+    const div2 = document.createElement("div");
+    div.classList.add("embedded-video");
+    const video = document.createElement("iframe");
+    video.src = song.songLink;
+    video.width = "560";
+    video.height = "315";
+    video.allowFullscreen = true;
+    div2.append(video);
+    artistContainer.append(div2);
+
+}
